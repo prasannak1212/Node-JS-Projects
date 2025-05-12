9c91cdfb66d9353ad75fa3bd11cfb75c2373cf33
prasannak1212 <prasanna12uo@gmail.com>
2025-05-12 00:13:10 +0000

    Modified inventory project

---
+++ Inventory Management/index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import productController from './src/controllers/products.controller.js'
+
+const server = express()
+const productControllers = new productController()
+
+server.get('/', productControllers.getProducts);
+
+server.use(express.static('src/views'))
+
+server.listen(5000, ()=>console.log('Server is Listening at 5000...'));
